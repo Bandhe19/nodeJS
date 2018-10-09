a7d38f0d4c2dcb5232a6b45579207c4ff6f12193
Nawaaz <nawaaz@Nawaazs-MacBook-Air.local>
2018-10-09 20:19:19 +0530

    Initial message

---
+++ puzzle.js
@@ -0,0 +1,32 @@
+var questions=[
+    'what is your name?\n',
+    'what is your hobby?\n',
+    'what is your fav?\n',
+];
+
+let answers=[];
+function ask(i)
+{
+    process.stdout.write(`\n ${questions[i]} \n`);
+    process.stdout.write('');
+}
+
+process.stdin.on('data',function(data)
+    {
+        console.log('\n Received data : '+data);
+        console.log('All answers :  '+answers);
+        answers.push(data.toString().trim());
+        if(answers.length<questions.length)
+        {
+            ask(answers.length);
+        }
+        else{
+            process.exit();
+        }
+    }
+);
+process.on('exit', function()
+{
+    process.stdout.write('Good bye....\n');
+});
+ask(0);
